4d2f7ac02a993aa1e5974f0593391a1f4adade6e
FlorentinaDili <73610425+FlorentinaDili@users.noreply.github.com>
2022-09-23 01:27:27 +0200

    Starting Search Bar' '

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,56 +1,96 @@
+import "./header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHotel } from "@fortawesome/free-solid-svg-icons"
+import { faCircle, faHotel, faHouse, 
+    faPlaceOfWorship, faQuestion, faQuestionCircle, faSearch, faShip, faTrain } from "@fortawesome/free-solid-svg-icons"
 import { faPlane } from "@fortawesome/free-solid-svg-icons"
 import { faCar } from "@fortawesome/free-solid-svg-icons"
 import { faTaxi } from "@fortawesome/free-solid-svg-icons"
 import { faCalendarDays } from "@fortawesome/free-solid-svg-icons"
 import { faPerson } from "@fortawesome/free-solid-svg-icons"
 
-import "./header.css";
+import { DateRange } from 'react-date-range';
+import { useState, setDate} from 'react'
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 const Header = () => {
+    const [date, setDtate] = useState([
+        {
+          startDate: new Date(),
+          endDate: null,
+          key: 'selection'
+        }
+      ]);
   return (
         <div className="header">
             <div className="headerContainer">
                 <div className="headerList">
                     <div className="headerListItem active">
-                    <FontAwesomeIcon icon={faHotel} />
-                    <span>Hotel</span>
+                    <FontAwesomeIcon icon={faHotel} className="headerListItemIcon"/>
+                    <span>Hotels</span>
                     </div>
                     <div className="headerListItem">
-                    <FontAwesomeIcon icon={faPlane} />
+                    <FontAwesomeIcon icon={faHouse} className="headerListItemIcon" />
+                    <span>Houses</span>
+                    </div>
+                    <div className="headerListItem">
+                    <FontAwesomeIcon icon={faShip} className="headerListItemIcon"/>
+                    <span>Ships</span>
+                    </div>
+                    <div className="headerListItem">
+                    <FontAwesomeIcon icon={faPlane} className="headerListItemIcon"/>
                     <span>Flights</span>
                     </div>
                     <div className="headerListItem">
-                    <FontAwesomeIcon icon={faCar} />
+                    <FontAwesomeIcon icon={faTrain} className="headerListItemIcon"/>
+                    <span>Trains</span>
+                    </div>
+                    <div className="headerListItem">
+                    <FontAwesomeIcon icon={faCar} className="headerListItemIcon"/>
                     <span>Car Rent</span>
                     </div>
                     <div className="headerListItem">
-                    <FontAwesomeIcon icon={faTaxi} />
+                    <FontAwesomeIcon icon={faTaxi} className="headerListItemIcon"/>
                     <span>Taxi</span>
+                    </div>
+                    <div className="headerListItem">
+                    <FontAwesomeIcon icon={faPlaceOfWorship} className="headerListItemIcon"/>
+                    <span>Attractions</span>
                     </div>
                 </div>
                 <h1 className="headerTitle">
-                   A lifetime of discounts? It's Genius.
+                   Looking for the best booking platform? You're on the right place.
                 </h1>
                  <p className="headerDesc">
                      Get rewarded for your travels – unlock instant savings of 10% or
                      more with a free alb-booking account
                 </p>
                 <button className="headerBtn"> Sign In / Register</button>
-                <div className="headerSearchItem">
-                    <FontAwesomeIcon icon={faHotel} />
-                    <input type="text"
-                     placeholder="Your destination"
-                     className="headerSearchInput" />
-                </div>
-                <div className="headerSearchItem">
-                    <FontAwesomeIcon icon={faCalendarDays} />
-                    <span className="headerSearchText">date to date</span>
-                </div>
-                <div className="headerSearchItem">
-                    <FontAwesomeIcon icon={faPerson} />
-                    <span className="headerSearchText">2 adults, 2 kids, 1 room</span>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faHotel} className="headerIcon"/>
+                        <input type="text"
+                        placeholder="Your destination"
+                        className="headerSearchInput" />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faCalendarDays} className="headerIcon"/>
+                        <span className="headerSearchText">date to date</span>
+                        <DateRange
+                        editableDateInputs={true}
+                        onChange={item => setDate([item.selection])}
+                        moveRangeOnFirstSelection={false}
+                        ranges={date}
+                        />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faPerson} className="headerIcon"/>
+                        <span className="headerSearchText">2 adults, 2 kids, 1 room</span>
+                    </div>
+                    <div className="headerSearchItem">
+                        <button className="headerBtn">Search</button>
+                        <FontAwesomeIcon icon={faSearch} className="headerSearchIcon"/>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import "./navbar.css"
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Navbar = () => {
   return (
@@ -6,6 +8,7 @@ const Navbar = () => {
         <div className="navContainer">
             <span className="logo">alb-booking</span>
             <div className="navItems">
+                    <FontAwesomeIcon icon={faQuestionCircle} />
                 <button className="navButton">Register</button>
                 <button className="navButton">Log In</button>
             </div>
