386ee715486cf9dc5820a0526690eae8b8bad66c
FlorentinaDili <73610425+FlorentinaDili@users.noreply.github.com>
2022-09-28 22:59:42 +0200

    Search Section /Hotels Page

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -17,41 +17,87 @@ export const List = () => {
   const [openDate, setOpenDate]=useState(false);
   const [options, setOptions]=useState(location.state.options);
 
-  return (
-    <div>
-    <Navbar />
-                {/*prop*/}
-    <Header type="list"/>
-      <div className="listContainer">
-        <div className="listWrapper">
+  
+    return (
+      <div>
+        <Navbar />
+        <Header type="list" />
+        <div className="listContainer">
+          <div className="listWrapper">
             <div className="listSearch">
-              <h1 className="lsTitle">Search
-              <FontAwesomeIcon icon={faSearch} className="searchIcon"/>
-              </h1>
+              <h1 className="lsTitle">Search</h1>
               <div className="lsItem">
                 <label>Destination</label>
-                <input type="text" placeholder={destination} />
+                <input placeholder={destination} type="text" />
               </div>
               <div className="lsItem">
-                <label>Check-In Date</label>
-                <span onClick={()=>setOpenDate(!openDate)}>
-                {`${format(date[0].startDate, "dd/MM/yyyy")}
-                to ${format(date[0].endDate, "dd/MM/yyyy")}`}
-                </span>
+                <label>Check-in Date</label>
+                <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                  date[0].startDate,
+                  "MM/dd/yyyy"
+                )} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+                {openDate && (
+                  <DateRange
+                    onChange={(item) => setDate([item.selection])}
+                    minDate={new Date()}
+                    ranges={date}
+                  />
+                )}
               </div>
               <div className="lsItem">
-                {openDate && <DateRange
-                onChange={item=>setDate([item.selection])} 
-                minDate={new Date()}
-                ranges={date}
-                />}
+                <label>Options</label>
+                <div className="lsOptions">
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">
+                      Min price <small>per night</small>
+                    </span>
+                    <input type="number" className="lsOptionInput" />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">
+                      Max price <small>per night</small>
+                    </span>
+                    <input type="number" className="lsOptionInput" />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Adult</span>
+                    <input
+                      type="number"
+                      min={1}
+                      className="lsOptionInput"
+                      placeholder={options.adult}
+                    />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Children</span>
+                    <input
+                      type="number"
+                      min={0}
+                      className="lsOptionInput"
+                      placeholder={options.children}
+                    />
+                  </div>
+                  <div className="lsOptionItem">
+                    <span className="lsOptionText">Room</span>
+                    <input
+                      type="number"
+                      min={1}
+                      className="lsOptionInput"
+                      placeholder={options.room}
+                    />
+                  </div>
+                </div>
               </div>
+              <button>Search</button>
             </div>
-            <div className="listResult"></div>
+            <div className="listResult">
+              
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    );
+  };
+  
 
 export default List
