43ecb393861eadfef317ac12bc142e40df04a0aa
FlorentinaDili <73610425+FlorentinaDili@users.noreply.github.com>
2022-09-28 20:58:50 +0200

    Starting List/Hotels page

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
+import list from './list.css'
 
 export const List = () => {
   return (
